334064a4d50e343f1cbbc9fe1bdd4696e7a06968
Jefferson Santos <jalexandre0@protonmail.com>
2021-03-03 15:37:19 -0300

    Exercicio 02 passou no teste

--- 02_both_ends.py
+++ 02_both_ends.py
@@ -8,8 +8,10 @@ for menor que 2, retorne uma string vazia.
 """
 
 def both_ends(s):
-    # +++ SUA SOLUÇÃO +++
-    return
+    if len(s) < 2:
+        s = ''
+    s = s[0:2] + s[-2:]
+    return s 
 
 
 # --- Daqui para baixo são apenas códigos auxiliáries de teste. ---
