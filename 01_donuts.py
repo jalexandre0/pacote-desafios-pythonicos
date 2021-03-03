34b8cf26d2734edaf48a07b8da26528f9b6dbc51
Jefferson Santos <jalexandre0@protonmail.com>
2021-03-03 15:29:46 -0300

    Desafio 1 passou nos testes.

--- 01_donuts.py
+++ 01_donuts.py
@@ -10,8 +10,10 @@ e donuts(23) retorna 'Number of donuts: many'
 """
 
 def donuts(count):
-    # +++ SUA SOLUÇÃO +++
-    return
+    if count >= 10:
+        count = "many"
+    msg = "Number of donuts: " + str(count)
+    return msg
 
 
 # --- Daqui para baixo são apenas códigos auxiliáries de teste. ---
